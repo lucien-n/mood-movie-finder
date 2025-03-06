2b3622517eeb8ac4d3f6bfb98776a686ee553304
Lucien <lucien.neuhoff@protonmail.com>
2025-03-06 10:14:07 +0100

    fixed path resolving

--- back/src/middlewares/swagger.ts
+++ back/src/middlewares/swagger.ts
@@ -1,6 +1,6 @@
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
-import docs from "../swagger/docs.json";
+import docs from "@/swagger/docs.json";
 
 export default (app: Express) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(docs));
