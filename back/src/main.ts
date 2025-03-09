d563ae6ccefc58654a15444bee7fc2da1fe5238b
Lucien <lucien.neuhoff@protonmail.com>
2025-03-09 23:50:35 +0100

    added helmet

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalErrorFilter } from './filters/global.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.enableCors({ origin: ['http://localhost:3001'] });
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new GlobalErrorFilter());
+  app.use(helmet());
 
   await app.listen(process.env.PORT ?? 3000);
 }
