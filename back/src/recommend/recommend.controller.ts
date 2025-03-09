7fa1e5d52cb34bfa402b2109a119d6630e8bd1e3
Lucien <lucien.neuhoff@protonmail.com>
2025-03-09 23:49:12 +0100

    feat: nestjs throttler

--- back/src/recommend/recommend.controller.ts
+++ back/src/recommend/recommend.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiErrorCode } from 'common';
+import { Throttle } from '@nestjs/throttler';
 import { RecommendService } from './recommend.service';
 
 @Controller('recommend')
 export class RecommendController {
   constructor(private readonly recommendService: RecommendService) {}
 
+  @Throttle({ default: { limit: 7, ttl: 1000 * 10 } })
   @Get('/:city')
   findManyByCity(@Param('city') city: string) {
     return this.recommendService.findManyByCity(city);
